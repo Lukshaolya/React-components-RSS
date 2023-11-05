e393fe518066fddd0b4240165ab6615e065e14f8
Lukshaolya <lukshaolya98@gmail.com>
2023-11-05 19:31:57 +0300

    feat: ErrorBoundary

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,12 @@
+import ErrorBoundary from './pages/ErrorBoundaty';
+import SearchPage from './pages/SearchPage';
+
 function App() {
-  return <p>Hello</p>;
+  return (
+    <ErrorBoundary>
+      <SearchPage />
+    </ErrorBoundary>
+  );
 }
 
 export default App;
