6377a595fb6ba1486cecc9bd0a23a87d1fcf2211
Lukshaolya <lukshaolya98@gmail.com>
2023-11-08 22:58:46 +0300

    feat: cancel Router

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,7 @@
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import SearchPage from './pages/SearchPage';
 
 function App() {
-  return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<SearchPage />} />
-      </Routes>
-    </BrowserRouter>
-  );
+  return <SearchPage />;
 }
 
 export default App;
